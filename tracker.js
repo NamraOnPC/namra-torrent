785f33371ffd98d0c2a1ac579ee2740e5576fdc8
Namra Fanse <35749104+NamraOnPC@users.noreply.github.com>
2020-11-04 09:52:53 -0500

    added connection request and response functions

--- tracker.js
+++ tracker.js
@@ -3,6 +3,8 @@
 const dgram = require('dgram');
 const Buffer = require('buffer').Buffer;
 const urlParse = require('url').parse;
+// crypto module to help us create a random number for our buffer
+const crypto = require('crypto');
 
 module.exports.getPeers = (torrent, callback) => {
     /*
@@ -53,11 +55,45 @@ function responseType(response){
 }
 
 function buildConnectionRequest(){
-    // TODO
+    /*
+        BitTorrent request protocol
+
+        Offset  Size            Name            Value
+        0       64-bit integer  connection_id   0x41727101980
+        8       32-bit integer  action          0 // connect
+        12      32-bit integer  transaction_id  ? // random
+        16
+
+        referenced from http://www.bittorrent.org/beps/bep_0015.html
+    */
+    
+    //  create a new empty buffer with a size of 16 bytes
+    const buf = Buffer.alloc(16);
+    
+    /* 
+        connection id
+        method writeUInt32BE writes an unsigned 32-bit integer in big-endian format
+        written in 4 byte chunks because there is no method to write a 64 bit integer
+    */
+    buf.writeUInt32BE(0x417, 0);
+    buf.writeUInt32BE(0x27101980, 4);
+
+    //action
+    buf.writeUInt32BE(0,8);
+    //transaction id
+    crypto.randomBytes(4).copy(buf, 12);
+    
+    return buf;
 }
 
 function parseConnectionResponse(response){
-    // TODO 
+    return {
+        // reading the action and the transaction id as unsigned 32 bit big-endian integers, passing the offset
+        action: response.readUInt32BE(0),
+        transactionId: response.readUInt32BE(4),
+        //  slice method to get the last 8 bytes. Since a 64-bit integer can' t be read.        
+        connectionId: response.slice(8)
+    }
 }
 
 function buildAnnounceRequest(connectionId){
