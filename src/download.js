0207a46cdba894beac145a681b280c4d777702ef
Namra Fanse <35749104+NamraOnPC@users.noreply.github.com>
2020-11-08 13:36:03 -0500

    fixed handshake handler

--- src/download.js
+++ src/download.js
@@ -20,6 +20,23 @@ function download(peer) {
     onWholeMsg(socket, msg => msgHandler(msg, socket));
 }
 
+function onWholeMsg(socket, callback) {
+  let savedBuf = Buffer.alloc(0);
+  let handshake = true;
+
+  socket.on('data', recvBuf => {
+    // msgLen calculates the length of a whole message
+    const msgLen = () => handshake ? savedBuf.readUInt8(0) + 49 : savedBuf.readInt32BE(0) + 4;
+    savedBuf = Buffer.concat([savedBuf, recvBuf]);
+
+    while (savedBuf.length >= 4 && savedBuf.length >= msgLen()) {
+      callback(savedBuf.slice(0, msgLen()));
+      savedBuf = savedBuf.slice(msgLen());
+      handshake = false;
+    }
+  });
+}
+
 function msgHandler(msg, socket) {
   if (isHandshake(msg)) socket.write(message.buildInterested());
 }
