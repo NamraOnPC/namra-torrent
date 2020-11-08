3cc0947922505006ed6a6490e8a5daa5e26367e0
Namra Fanse <35749104+NamraOnPC@users.noreply.github.com>
2020-11-08 14:53:40 -0500

    added choke and unchoke handlers

--- src/download.js
+++ src/download.js
@@ -4,22 +4,23 @@ const net = require('net');
 const Buffer = require('buffer').Buffer;
 const tracker = require('./tracker');
 const message = require('./message');
+const Pieces = require('./Pieces');
 
 module.exports =  torrent => {
-  const requested = [];
     tracker.getPeers(torrent, peers => {
-        peers.forEach(peer => download(peer, torrent,requested));
+        const pieces = new Pieces(torrent.info.pirces.length / 20);
+        peers.forEach(peer => download(peer, torrent, pieces));
     });
 };
 
-function download(peer, torrent, requested) {
-    const socket = net.Socket();
+function download(peer, torrent, pieces) {
+    const socket = new net.Socket();
     socket.on('error', console.log);
     socket.connect(peer.port, peer.ip, () => {
       socket.write(message.buildHandshake(torrent));
     });
-    const queue = []
-    onWholeMsg(socket, msg => msgHandler(msg, socket,requested , queue));
+    const queue = {choked: true, queue: []};
+    onWholeMsg(socket, msg => msgHandler(msg, socket, pieces, queue));
 }
 
 function onWholeMsg(socket, callback) {
@@ -40,48 +41,52 @@ function onWholeMsg(socket, callback) {
 }
 
 // function will check what kind of message we are receiving and handle it accordingly. 
-function msgHandler(msg, socket) {
+function msgHandler(msg, socket, pieces, queue) {
   if (isHandshake(msg)){ 
     socket.write(message.buildInterested());
   }else{
     const m = message.parse(msg);
 
     if (m.id === 0) chokeHandler();
-    if (m.id === 1) unchokeHandler();
-    if (m.id === 4) haveHandler(m.payload, socket, requested, queue);
+    if (m.id === 1) unchokeHandler(socket, pieces, queue);
+    if (m.id === 4) haveHandler(m.payload);
     if (m.id === 5) bitfieldHandler(m.payload);
-    if (m.id === 7) pieceHandler(m.payload, socket, requested, queue);
+    if (m.id === 7) pieceHandler(m.payload);
   }
 }
 
-function chokeHandler() { ... }
-
-function unchokeHandler() { ... }
-
-function haveHandler(payload, socket, requested, queue) {
-  // ...
-  const pieceIndex = payload.readUInt32BE(0);
-  queue.push(pieceIndex);
-  if (queue.length === 1) {
-    requestPiece(socket, requested, queue);
-  }
+function chokeHandler(socket) { 
+  socket.end();
 }
 
-function bitfieldHandler(payload) { ... }
-
-function pieceHandler(payload, socket, requested, queue) {
-  // ...
-  queue.shift();
-  requestPiece(socket, requested, queue);
+function unchokeHandler(socket, pieces, queue) { 
+  queue.choked = false;
+  requestPiece(socket, pieces, queue);
 }
 
-nction requestPiece(socket, requested, queue) {
-  if (requested[queue[0]]) {
-    queue.shift();
-  } else {
-    // this is pseudo-code, as buildRequest actually takes slightly more
-    // complex arguments
-    socket.write(message.buildRequest(pieceIndex));
+function haveHandler() {
+  // TODO
+}
+
+function bitfieldHandler() { 
+  // TODO
+}
+
+function pieceHandler() {
+  // TODO
+}
+
+function requestPiece(socket, pieces, queue) {
+  if (queue.choked) return null;
+
+  while (queue.queue.length) {
+    const pieceIndex = queue.shift();
+    if (pieces.needed(pieceIndex)) {
+      // need to fix this
+      socket.write(message.buildRequest(pieceIndex));
+      pieces.addRequested(pieceIndex);
+      break;
+    }
   }
 }
 
